da857a987c4648af9670c3bfd4ae8b05cd0ef00f
adpandey02 <adpandey2102003@gmail.com>
2023-11-02 22:39:00 +0530

    project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AccidentResponseSystem',
+    version= '0.0.0',
+    author= 'Aditya Pandey',
+    author_email= 'adpandey2102003@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
---
+++ test.py
@@ -0,0 +1,2 @@
+import zipfile
+print(DATA_INGESTION_DIR_NAME)

a7f4c396bf9346abcfe65c1908221de1847455de
adpandey02 <adpandey2102003@gmail.com>
2023-11-03 20:06:31 +0530

    all tasks complete

--- Detection/utils/main_utils.py
+++ Detection/utils/main_utils.py
@@ -36,6 +36,21 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
     
 
 
+def correct_paths_in_yaml(yaml_file_path):
+    # Load the YAML file
+    with open(yaml_file_path, 'r') as file:
+        config = yaml.safe_load(file)
+
+    # Update parameters
+    config['test'] = 'D:\\projects\\DL\\AccidentResponseSystem\\artifacts\\data_ingestion\\test\\images'
+    config['train'] = 'D:\\projects\\DL\\AccidentResponseSystem\\artifacts\\data_ingestion\\train\\images'
+    config['val'] = 'D:\\projects\\DL\\AccidentResponseSystem\\artifacts\\data_ingestion\\valid\\images'
+
+    # Write the updated content back to the file
+    with open(yaml_file_path, 'w') as file:
+        yaml.dump(config, file)
+
+
 
 def decodeImage(imgstring, fileName):
     imgdata = base64.b64decode(imgstring)
